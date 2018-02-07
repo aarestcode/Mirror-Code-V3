4c54dcc081dec504e1669b1fb995f107797ab6f6
aarestcode <aarestcode@gmail.com>
2018-02-06 17:53:01 -0800

    Electrode post ramp up 2
    Keep deactivated electrodes deactivated after ramp up + minor code
    corrections

--- src/electrode_actuation.c
+++ src/electrode_actuation.c
@@ -182,7 +182,7 @@ int StartElectrodeActuation(void){
 		
 		error = ChannelOff(ch);  // Start charging channel
 		
-		REGISTER[memory_ELECTRODE1 + ch] == (0x0A<<16) + REGISTER[memory_HV_BIAS] // Set electrode to 0V for 10ms
+		if(REGISTER[memory_ELECTRODE1 + ch]) REGISTER[memory_ELECTRODE1 + ch] = (0xA0000) + REGISTER[memory_HV_BIAS]; // Set electrode to 0V for 10ms
 	}
 	if(error) return error;
 	
